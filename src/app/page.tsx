114fe267324dff308ae923c19aa38534a65213e1
LeanderTS <leosorte87@gmail.com>
2026-09-08 10:52:59 +0200

    Update page.tsx
    Update frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default async function HomePage({
       <Hero />
 
       <div className="mb-4">
-        <h2 className="text-xl font-semibold">Søk etter disk </h4>
+        <h2 className="text-xl font-semibold">Søk etter disk</h2>
       </div>
 
       <SearchFilters searchParams={searchParams} />
