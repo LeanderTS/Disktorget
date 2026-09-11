36b3a8989c064f53377dfb7f19305963e2c5cd27
LeanderTS <leosorte87@gmail.com>
2026-09-11 10:31:39 +0200

    Update NavbarShell.tsx

--- src/components/NavbarShell.tsx
+++ src/components/NavbarShell.tsx
@@ -13,7 +13,7 @@ export default function NavbarShell({
       <div className="mx-auto flex max-w-6xl items-center gap-4 px-4 py-3">
         <Link href="/" className="flex flex-shrink-0 items-center">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/Disktorget Logo.png" alt="Disktorget" className="h-12 w-auto" />
+          <img src="/Disktorget Logo.png" alt="Disktorget" className="h-20 w-auto" />
         </Link>
 
         <form method="get" action="/" className="mx-auto w-full max-w-md flex-1">
