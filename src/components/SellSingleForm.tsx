ce237ee358ebd9420caa3986d970d30cc1977d85
LeanderTS <leosorte87@gmail.com>
2026-09-15 19:00:21 +0200

    Update SellSingleForm.tsx

--- src/components/SellSingleForm.tsx
+++ src/components/SellSingleForm.tsx
@@ -224,7 +224,7 @@ export default function SellSingleForm({ userId }: { userId: string }) {
               onChange={(e) => setIsHbo(e.target.checked)}
               className="h-4 w-4"
             />
-            HBO (Høyeste bud overtar)
+            HBO
           </label>
         </div>
       </div>
